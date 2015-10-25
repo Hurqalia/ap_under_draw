9f9d132069383422abcb843fce41535b1c67ec4e
Hurqalia <Hurqalia@users.noreply.github.com>
2015-10-25 20:52:51 +0100

    Create ap_under_draw.meta.js

---
+++ ap_under_draw.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @id             iitc-plugin-apunderdraw
+// @name           IITC plugin: Show AP under draw.
+// @author         hurqalia22
+// @category       Info
+// @version        0.1.1.20151025.001
+// @namespace      https://github.com/Hurqalia/ap_under_draw
+// @updateURL      https://github.com/Hurqalia/ap_under_draw/raw/master/ap_under_draw.meta.js
+// @downloadURL    https://github.com/Hurqalia/ap_under_draw/raw/master/ap_under_draw.user.js
+// @installURL     https://github.com/Hurqalia/ap_under_draw/raw/master/ap_under_draw.user.js
+// @description    [hurqalia22-2015-10-25-001] Show AP under draw.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
