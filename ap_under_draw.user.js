ad031172c222ad132baf48c320f838deda3d8c63
Keith Kyzivat <kamaji@gmail.com>
2016-09-11 22:56:08 -0400

    Revert "Reverse enl/res outputs - counts were reversed."
    This reverts commit 7b1d5d76942d659b4a226673828018d8e64292e9.
    
    Oops! It was right the first time - "Enemy portals"... not our own
    portals...

--- ap_under_draw.user.js
+++ ap_under_draw.user.js
@@ -254,8 +254,8 @@ function wrapper(plugin_info) {
             var t = this;
 
             var template_table = function( team, datas ) {
-                var title_team  = (team == 'R') ? 'Resistance' : 'Enlightened';
-                var table_color = (team == 'R') ? 'blue' : 'green';
+                var title_team  = (team == 'R') ? 'an Enlightened' : 'a Resistant';
+                var table_color = (team == 'R') ? 'green' : 'blue';
                 var html = "<table class='audtable' style='margin-bottom:5px; border: 1px solid #20A8B1;'>" 
                 + "<thead style='background-color:" + table_color +";'><tr><th colspan='5'>" + title_team  + "</th></tr></thead>"
                 + "<tbody>"
