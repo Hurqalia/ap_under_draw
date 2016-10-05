01eebe2f5d67fbee9a35e7748fdd749e48397c42
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:27:19 +0200

    Update ap_under_draw.meta.js

--- ap_under_draw.meta.js
+++ ap_under_draw.meta.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: Show AP under a polygon/draw area.
 // @author         hurqalia22, keithel
 // @category       Info
-// @version        0.1.6.20160914.002
+// @version        0.1.7.20160914.002
 // @namespace      https://github.com/Hurqalia/ap_under_draw
 // @updateURL      https://raw.githubusercontent.com/Hurqalia/ap_under_draw/master/ap_under_draw.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Hurqalia/ap_under_draw/master/ap_under_draw.user.js
